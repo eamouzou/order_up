fc75eff1227230e7ab82fb10bc50d347367d9091
Elom Amouzou <elom1990@yahoo.com>
2020-02-21 11:11:01 -0700

    add unecessarily convoluted activerecord query for chef instance method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,8 +1,9 @@
-class Chef <ApplicationRecord
+class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
   def get_all_ingredients
-    require "pry"; binding.pry
+    # dishes.first.ingredients.pluck(:name)
+    Ingredient.where(id: DishIngredient.where(dish_id: dishes.pluck(:id)).pluck(:ingredient_id)).pluck(:name)
   end
 end
