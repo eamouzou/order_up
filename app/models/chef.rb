813d423f38a6407dffb404cf1c6e88bdbf36ebe1
Elom Amouzou <elom1990@yahoo.com>
2020-02-21 11:47:52 -0700

    notes during review

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,7 +3,10 @@ class Chef < ApplicationRecord
   has_many :dishes
 
   def get_all_ingredients
-    # dishes.first.ingredients.pluck(:name)
+    # dishes.select('ingredients.*').joins(:ingredients).distinct #(mike's approach)
+    # with approach above, in your chef's ingredients index view, you change ingredients to ingredients.name
+    # because it returns dish objects with name attributes
+    # could also do: dishes.select('ingredients.name').joins(:ingredients).distinct
     Ingredient.where(id: DishIngredient.where(dish_id: dishes.pluck(:id)).pluck(:ingredient_id)).pluck(:name)
   end
 end
